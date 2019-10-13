76db309862bd152af7d90a0d56598a44158d88f0
Lastor <lastoraya@gmail.com>
2019-10-13 14:54:02 +0800

    [debug] URL 防重複機制，追加 SSL 比對

--- routes/home.js
+++ routes/home.js
@@ -24,11 +24,12 @@ router.post('/', async (req, res) => {
   if (!input) return res.render('index', { error: 'Please provide a valid URL' })
 
   const url = checkFormat(input)
+  const protocol = hasSSL(input)
   let short = randomstring.generate(5)
   let links = []
-  try { links = await Link.find({ $or: [{ short: short }, { url: url }] }).limit(2).exec() }
+  try { links = await Link.find({ $or: [{ short: short }, { url: url, ssl: protocol }] }).limit(2).exec() }
   catch (err) { console.error(err) }
-  
+
   // 確認 url 是否已存在
   const resURL = links.find(link => link.url === url)
   if (resURL) return res.render('index', { short: resURL.short, input })
@@ -39,7 +40,7 @@ router.post('/', async (req, res) => {
   }
   
   // save to database
-  Link.create({ short: short, url: url, ssl: hasSSL(input) })
+  Link.create({ short: short, url: url, ssl: protocol })
     .then(link => res.render('index', { short, input }))
     .catch(err => res.status(422).json(err))
 })
